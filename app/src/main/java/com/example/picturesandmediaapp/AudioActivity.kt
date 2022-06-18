173d1ac609892e42b42b2f03fdaf1e7e55cfd19f
fellipetav <43867160+fellipetav@users.noreply.github.com>
2022-06-18 19:34:38 -0300

    implement Audio and Picture Activities

---
+++ app/src/main/java/com/example/picturesandmediaapp/AudioActivity.kt
@@ -0,0 +1,54 @@
+package com.example.picturesandmediaapp
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import java.lang.Integer.parseInt
+import kotlin.random.Random
+
+lateinit var randNum: Random
+
+class AudioActivity : AppCompatActivity() {
+
+    lateinit var number: EditText
+    lateinit var guessBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio)
+
+        var sound : MediaPlayer = MediaPlayer.create(this, R.raw.sound)
+        number = findViewById(R.id.number)
+        guessBtn = findViewById(R.id.guessBtn)
+        setOnClickListenerOnGuessingButton(sound)
+    }
+
+    private fun setOnClickListenerOnGuessingButton(sound: MediaPlayer) {
+        guessBtn.setOnClickListener {
+            val randText = ""
+            val random: Int = randNum.nextInt(5) + 1
+            val userChoice: Int = parseInt(number.text.toString().trim())
+            if (userChoice < 1 || userChoice > 5) {
+                showToast("Please guess a number between 1 and 5")
+            } else if (userChoice == random) {
+                /*If User worked out guessing the number correctly
+                        Crowd Cheering sound will start*/
+                if (sound.isPlaying) {
+                    sound.seekTo(0)
+                } else {
+                    sound.start()
+                }
+                showToast("Congratulations, you guessed the number correctly")
+            } else {
+                showToast("Sorry, better luck next time!")
+            }
+        }
+    }
+
+    private fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+}
--- app/src/main/java/com/example/picturesandmediaapp/MainActivity.kt
+++ app/src/main/java/com/example/picturesandmediaapp/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.picturesandmediaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.picturesandmediaapp.PictureActivity
+import com.example.picturesandmediaapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    public override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    public override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //We have 2 different activities to Handle via Menus
+        var id = item.itemId
+        if (id == R.id.menu_game) {
+            val audioIntent = Intent(this, AudioActivity::class.java)
+            startActivity(audioIntent)
+        }
+        else if(id == R.id.menu_pictures){
+            val pictureIntent = Intent(this, PictureActivity::class.java)
+            startActivity(pictureIntent)
+        }
+        return true
     }
 }
